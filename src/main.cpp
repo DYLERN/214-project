e916dd979fffe61b84df7a1ba2ec3fbd65f4f397
DYLERN <dylan@squarepeg.co.za>
2020-11-06 11:43:39 +0200

    added logistics main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
+#include "pch.h"
+
+#include <random>
+
 #include "core/FormulaOneCar.h"
+#include "logistics/InventoryItem.h"
+#include "logistics/Container.h"
+#include "logistics/LogisticsManager.h"
 
 int main() {
+    std::srand(std::time(0));
+    auto plane = std::make_shared<Plane>("ADZ-903");
+    std::vector<std::shared_ptr<Truck> > trucks = {std::make_shared<Truck>("BG 64 UI"), std::make_shared<Truck>("WD 31 PD")};
+
+    Engine* engine = new Engine();
+    Chassis* chassis = new Chassis();
+    std::array<Tyre*, 4> tyres = {new HardTyre, new HardTyre, new HardTyre, new HardTyre};
+
+    auto car = std::make_shared<FormulaOneCar>(engine, chassis, tyres);
+    std::shared_ptr<InventoryItem> drill = std::make_shared<Equipment>("Drill", 10000, 5);
+    std::shared_ptr<InventoryItem> wrench = std::make_shared<Equipment>("Wrench", 10000, 6);
+    std::shared_ptr<InventoryItem> jack = std::make_shared<Equipment>("Jack", 5000, 10);
+
+    LogisticsManager manager(plane, trucks);
+    manager.setDestination(std::make_shared<Destination>("Spain", EUROPEAN));
+    manager.addCar(car);
+    manager.addInventoryItem(drill);
+    manager.addInventoryItem(wrench);
+    manager.addInventoryItem(jack);
+    manager.dispatch();
+
+    manager.joinAll();
+    
     return 0;
 }
